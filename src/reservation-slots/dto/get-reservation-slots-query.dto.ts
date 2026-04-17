26262e04af106b8d500b8e0a3c173ffa1763cf55
Kristian Kubik <898202+kristiankubik@users.noreply.github.com>
2026-04-17 18:54:20 +0200

    update optional fields for reservation-slots

--- src/reservation-slots/dto/get-reservation-slots-query.dto.ts
+++ src/reservation-slots/dto/get-reservation-slots-query.dto.ts
@@ -2,17 +2,19 @@ import { Type } from 'class-transformer';
 import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
 
 export class GetReservationSlotsQueryDto {
+    @IsOptional()
     @Type(() => Number)
     @IsInt()
     @Min(1900)
     @Max(2100)
-    year!: number;
+    year?: number;
 
+    @IsOptional()
     @Type(() => Number)
     @IsInt()
     @Min(1)
     @Max(12)
-    month!: number;
+    month?: number;
 
     @IsOptional()
     @Type(() => Number)
